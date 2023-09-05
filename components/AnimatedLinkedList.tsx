923c380cb136ef13bfb82a95e40b8aaa875b344f
Jake Rintamaki <jakerintamaki771@jakes-mbp.attlocal.net>
2023-09-05 14:09:31 -0400

    AnimatedLinkedList and Lawofdominance mobile component update

--- components/AnimatedLinkedList.tsx
+++ components/AnimatedLinkedList.tsx
@@ -1,4 +1,4 @@
-"use client"; 
+"use client";
 
 import React, { useState, useEffect, useRef } from "react";
 import * as d3 from "d3";
@@ -6,29 +6,31 @@ import * as d3 from "d3";
 interface NodeDatum {
   id: string;
   label: string;
-  x: number;
-  y: number;
+  x: number; // Relative position (0-1)
+  y: number; // Relative position (0-1)
 }
 
 const AnimatedLinkedList: React.FC = () => {
   const [nodes, setNodes] = useState<NodeDatum[]>([
-    { id: "1", label: "Step 1", x: 80, y: 80 },
+    { id: "1", label: "Step 1", x: 0.1, y: 0.1 },
   ]);
   const ref = useRef<SVGSVGElement>(null);
 
   useEffect(() => {
     const svg = d3.select(ref.current);
+    const { clientWidth, clientHeight } = svg.node() as unknown as HTMLElement;
+
     svg.selectAll("circle").data(nodes).join("circle")
-      .attr("cx", d => d.x)
-      .attr("cy", d => d.y)
+      .attr("cx", d => d.x * clientWidth)
+      .attr("cy", d => d.y * clientHeight)
       .attr("r", 50)
       .attr("stroke", "green")
       .attr("fill", "none")
       .attr("stroke-width", 3);
 
     svg.selectAll("text").data(nodes).join("text")
-      .attr("x", d => d.x)
-      .attr("y", d => d.y)
+      .attr("x", d => d.x * clientWidth)
+      .attr("y", d => d.y * clientHeight)
       .attr("text-anchor", "middle")
       .attr("dominant-baseline", "central")
       .attr("style", "font: 24px sans-serif;")
@@ -36,10 +38,10 @@ const AnimatedLinkedList: React.FC = () => {
       .text(d => d.label);
 
     svg.selectAll("line").data(nodes.slice(0, -1)).join("line")
-      .attr("x1", (d, i) => d.x + 50)
-      .attr("y1", d => d.y)
-      .attr("x2", (d, i) => nodes[i + 1].x - 50)
-      .attr("y2", (d, i) => nodes[i + 1].y)
+      .attr("x1", (d, i) => d.x * clientWidth + 50)
+      .attr("y1", d => d.y * clientHeight)
+      .attr("x2", (d, i) => nodes[i + 1].x * clientWidth - 50)
+      .attr("y2", (d, i) => nodes[i + 1].y * clientHeight)
       .attr("stroke", "black")
       .attr("stroke-width", 3)
       .attr("marker-end", "url(#arrow)");
@@ -51,8 +53,8 @@ const AnimatedLinkedList: React.FC = () => {
       const newNode = {
         id: `${newId}`,
         label: `Step ${newId}`,
-        x: 80 + (newId - 1) * 120,
-        y: 80,
+        x: 0.1 + (newId - 1) * 0.1,
+        y: 0.1,
       };
       setNodes([...nodes, newNode]);
     }
@@ -66,33 +68,13 @@ const AnimatedLinkedList: React.FC = () => {
 
   return (
     <div>
-      <button onClick={addNode} style={{
-        position: 'relative',
-        left: '75px',
-        top: '210px',
-        background: 'linear-gradient(to right, #004400, #006600, #228B22, #44AA44, #66CC66)',
-        fontSize: '24px',
-        color: 'white',
-        border: 'none',
-        padding: '15px 32px',
-        cursor: 'pointer'
-      }}>
+      <button onClick={addNode} className="increment-button">
         Increment
       </button>
-      <button onClick={removeNode} style={{
-        position: 'relative',
-        left: '125px',
-        top: '210px',
-        background: 'linear-gradient(to right, #004400, #006600, #228B22, #44AA44, #66CC66)',
-        fontSize: '24px',
-        color: 'white',
-        border: 'none',
-        padding: '15px 32px',
-        cursor: 'pointer'
-      }}>
+      <button onClick={removeNode} className="decrement-button">
         Decrement
       </button>
-      <svg ref={ref} width={1000} height={500}>
+      <svg ref={ref} width="100%" height="100%" viewBox="0 0 1000 500">
         <defs>
           <marker id="arrow" viewBox="0 0 10 10" refX="9" refY="5"
             markerWidth="6" markerHeight="6" orient="auto">
@@ -100,6 +82,22 @@ const AnimatedLinkedList: React.FC = () => {
           </marker>
         </defs>
       </svg>
+      <style jsx>{`
+        .increment-button, .decrement-button {
+          font-size: 24px;
+          background: linear-gradient(to right, #004400, #006600, #228B22, #44AA44, #66CC66);
+          color: white;
+          border: none;
+          padding: 15px 32px;
+          cursor: pointer;
+        }
+        @media (max-width: 768px) {
+          .increment-button, .decrement-button {
+            font-size: 18px;
+            padding: 10px 20px;
+          }
+        }
+      `}</style>
     </div>
   );
 };
--- components/Lawofdominance.tsx
+++ components/Lawofdominance.tsx
@@ -6,7 +6,6 @@ const calculateDominantRecessive = (
   allele1Parent1: string,
   allele1Parent2: string
 ): string => {
-  // Assuming 'A' is dominant and 'a' is recessive
   return allele1Parent1 === 'A' || allele1Parent2 === 'A' ? 'A' : 'a';
 };
 
@@ -16,24 +15,10 @@ const LawOfDominance: React.FC = () => {
 
   const result = calculateDominantRecessive(allele1Parent1, allele1Parent2);
 
-  const outerBoxStyle: React.CSSProperties = {
-    border: '2px solid black',
-    borderRadius: '5px',
-    padding: '10px',
-    width: '50%',
-    margin: 'auto',
-  };
-
-  const twoByTwoGridStyle: React.CSSProperties = {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(2, 1fr)',
-    gap: '10px',
-  };
-
   return (
-    <div style={outerBoxStyle}>
+    <div className="outer-box">
       <h1>Law of Dominance </h1>
-      <div style={twoByTwoGridStyle}>
+      <div className="two-by-two-grid">
         {['Parent 1', 'Parent 2'].map((parent, pIdx) => (
           <div key={parent}>
             <label>{`${parent}: `}</label>
@@ -49,6 +34,29 @@ const LawOfDominance: React.FC = () => {
       </div>
       <h2>Resulting Trait</h2>
       <div>{`The dominant trait is: ${result}`}</div>
+      <style jsx>{`
+        .outer-box {
+          border: 2px solid black;
+          border-radius: 5px;
+          padding: 10px;
+          width: 50%;
+          margin: auto;
+        }
+        .two-by-two-grid {
+          display: grid;
+          grid-template-columns: repeat(2, 1fr);
+          gap: 10px;
+        }
+        @media (max-width: 768px) {
+          .outer-box {
+            width: 95%;
+            padding: 5px;
+          }
+          .two-by-two-grid {
+            grid-template-columns: repeat(1, 1fr);
+          }
+        }
+      `}</style>
     </div>
   );
 };
