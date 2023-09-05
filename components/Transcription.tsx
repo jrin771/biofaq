6f90048ce65ba51cef9023b113ed142e1babc826
Jake Rintamaki <jakerintamaki771@jakes-mbp.attlocal.net>
2023-09-04 23:08:37 -0400

    added hbonds and repressor/transcription factors

--- components/Transcription.tsx
+++ components/Transcription.tsx
@@ -25,10 +25,25 @@ const transcriptionMap: TranscriptionMap = {
   'G': 'C'
 };
 
+const complementMap: TranscriptionMap = {
+  'A': 'T',
+  'T': 'A',
+  'C': 'G',
+  'G': 'C'
+};
+
 const Transcription: React.FC = () => {
   const [dnaIndex, setDnaIndex] = useState<number>(0);
+  const [speed, setSpeed] = useState<number>(150);
   const [rnaStrand, setRnaStrand] = useState<string[]>([]);
-  const dnaStrand: string[] = Array.from({ length: 30 }, (_, i) => ['A', 'T', 'C', 'G'][i % 4]);
+  const [dnaStrand, setDnaStrand] = useState<string[]>([]);
+  const [complementStrand, setComplementStrand] = useState<string[]>([]);
+
+  useEffect(() => {
+    const newDnaStrand: string[] = Array.from({ length: 100 }, (_, i) => ['A', 'T', 'C', 'G'][Math.floor(Math.random() * 4)]);
+    setDnaStrand(newDnaStrand);
+    setComplementStrand(newDnaStrand.map((base) => complementMap[base]));
+  }, []);
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -40,14 +55,28 @@ const Transcription: React.FC = () => {
         setDnaIndex(0);
         setRnaStrand([]);
       }
-    }, 50);
+    }, speed);
 
     return () => clearInterval(timer);
-  }, [dnaIndex]);
+  }, [dnaIndex, speed, dnaStrand]);
 
-  const renderBase = (base: string, index: number) => (
-    <span key={index} style={{ color: baseColors[base], fontSize: '2em' }}>
+  const renderBase = (base: string) => (
+    <span style={{ color: baseColors[base], fontSize: '2em' }}>
       {base}
+    </span>
+  );
+
+  const renderBond = () => (
+    <span style={{ color: 'black', fontSize: '1.5em', margin: '0' }}>
+      |
+    </span>
+  );
+
+  const renderBasePair = (base: string, index: number) => (
+    <span key={index} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '0' }}>
+      {renderBase(complementMap[base])}
+      {renderBond()}
+      {renderBase(base)}
     </span>
   );
 
@@ -62,16 +91,25 @@ const Transcription: React.FC = () => {
     alignItems: 'center'
   };
 
+  const changeSpeed = (newSpeed: number) => {
+    setSpeed(newSpeed);
+  };
+
   return (
-    <div>
+      <div>
+        <div style={{ padding: '20px', backgroundColor: '#eee', textAlign: 'center' }}>
+          <button onClick={() => changeSpeed(250)} style={{ margin: '5px', padding: '10px', fontSize: '1em' }}>Repressor</button>
+          <button onClick={() => changeSpeed(150)} style={{ margin: '5px', padding: '10px', fontSize: '1em' }}>Normal</button>
+          <button onClick={() => changeSpeed(50)} style={{ margin: '5px', padding: '10px', fontSize: '1em' }}>Transcription Factor</button>
+        </div>
       <div style={centeredStyle}>
-        <span style={labelStyle}>DNA: </span>
-        {dnaStrand.map((base, index) => renderBase(base, index))}
+        <span style={labelStyle}>DNA Base Pairs: </span>
+        {dnaStrand.map((base, index) => renderBasePair(base, index))}
       </div>
       <div style={centeredStyle}>
         <span style={labelStyle}>RNA: </span>
-        {Array.from({ length: 30 }, (_, i) => (
-          i < rnaStrand.length ? renderBase(rnaStrand[i], i) : <span key={i} style={{ fontSize: '2em' }}> </span>
+        {Array.from({ length: 100 }, (_, i) => (
+          i < rnaStrand.length ? renderBase(rnaStrand[i]) : <span key={i} style={{ fontSize: '2em' }}> </span>
         ))}
         <span style={{ fontSize: '2em', visibility: dnaIndex < dnaStrand.length ? 'visible' : 'hidden' }}>RNAP</span>
       </div>
